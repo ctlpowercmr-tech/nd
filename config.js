28489810b03cf898860551ea2d19d74161f226dd
ctlpowercmr-tech <ctlpowercmr@gmail.com>
2025-11-09 14:18:05 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,3 @@
 const CONFIG = {
   API_URL: 'https://ns-1-72e7.onrender.com' // Votre URL Render
 };
-
-const MESSAGES_VOCAUX = {
-  selection: 'Boisson sélectionnée avec succès',
-  paiement: 'Veuillez scanner le QR code avec votre téléphone ou utiliser cet ID de transaction',
-  reussite: 'Paiement réussi! Votre commande sera prête dans 4 secondes',
-  erreur: 'Erreur de connexion au serveur'
-};
-
-
-
