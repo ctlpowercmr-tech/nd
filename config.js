cc572d33536cd6b36a9d38bae0ddf5a4bd7aee8b
ctlpowercmr-tech <ctlpowercmr@gmail.com>
2025-11-09 12:58:30 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  API_URL: 'https://ns-fjzl.onrender.com' // Votre URL Render
+  API_URL: 'https://ns-0apc.onrender.com' // Votre URL Render
 };
 
 const MESSAGES_VOCAUX = {
@@ -8,3 +8,4 @@ const MESSAGES_VOCAUX = {
   reussite: 'Paiement réussi! Votre commande sera prête dans 4 secondes',
   erreur: 'Erreur de connexion au serveur'
 };
+
