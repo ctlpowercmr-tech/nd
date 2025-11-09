ad7e1b0fa2f330749261a6b96bfa78ec8cb863b9
ctlpowercmr-tech <ctlpowercmr@gmail.com>
2025-11-09 16:45:20 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,63 +1,61 @@
 const CONFIG = {
   API_URL: 'https://votre-api.render.com',
-  APP_NAME: 'DISTRIBUTEUR 3.0'
+  BOISSONS: [
+    { 
+      id: 1, 
+      nom: "Coca-Cola 33cl", 
+      prix: 500, 
+      image: "https://images.unsplash.com/photo-1622483767028-3f66f32aef97?w=400&h=400&fit=crop",
+      categorie: "soda"
+    },
+    { 
+      id: 2, 
+      nom: "Pepsi 33cl", 
+      prix: 500, 
+      image: "https://images.unsplash.com/photo-1624555130581-1d9cca8a8a82?w=400&h=400&fit=crop",
+      categorie: "soda"
+    },
+    { 
+      id: 3, 
+      nom: "Fanta Orange 33cl", 
+      prix: 500, 
+      image: "https://images.unsplash.com/photo-1624555130581-1d9cca8a8a82?w=400&h=400&fit=crop",
+      categorie: "soda"
+    },
+    { 
+      id: 4, 
+      nom: "Sprite 33cl", 
+      prix: 500, 
+      image: "https://images.unsplash.com/photo-1624555130581-1d9cca8a8a82?w=400&h=400&fit=crop",
+      categorie: "soda"
+    },
+    { 
+      id: 5, 
+      nom: "Coca-Cola 50cl", 
+      prix: 700, 
+      image: "https://images.unsplash.com/photo-1622483767028-3f66f32aef97?w=400&h=400&fit=crop",
+      categorie: "soda"
+    },
+    { 
+      id: 6, 
+      nom: "Pepsi 50cl", 
+      prix: 700, 
+      image: "https://images.unsplash.com/photo-1624555130581-1d9cca8a8a82?w=400&h=400&fit=crop",
+      categorie: "soda"
+    },
+    { 
+      id: 7, 
+      nom: "Energy Drink 25cl", 
+      prix: 800, 
+      image: "https://images.unsplash.com/photo-1624555130581-1d9cca8a8a82?w=400&h=400&fit=crop",
+      categorie: "energy"
+    },
+    { 
+      id: 8, 
+      nom: "Jus d'Orange 1L", 
+      prix: 1000, 
+      image: "https://images.unsplash.com/photo-1622483767028-3f66f32aef97?w=400&h=400&fit=crop",
+      categorie: "jus"
+    }
+  ]
 };
-
-const PRODUITS_DEFAUT = [
-  {
-    id: 1,
-    nom: "Coca-Cola 33cl",
-    prix: 500,
-    image: "https://images.unsplash.com/photo-1622483767028-3f66f32aef97?w=400&h=400&fit=crop",
-    categorie: "soda"
-  },
-  {
-    id: 2,
-    nom: "Pepsi 33cl",
-    prix: 450,
-    image: "https://images.unsplash.com/photo-1624555130581-1d9cca1c731d?w=400&h=400&fit=crop",
-    categorie: "soda"
-  },
-  {
-    id: 3,
-    nom: "Fanta Orange 33cl",
-    prix: 450,
-    image: "https://images.unsplash.com/photo-1624555130581-1d9cca1c731d?w=400&h=400&fit=crop",
-    categorie: "soda"
-  },
-  {
-    id: 4,
-    nom: "Sprite 33cl",
-    prix: 450,
-    image: "https://images.unsplash.com/photo-1624555130581-1d9cca1c731d?w=400&h=400&fit=crop",
-    categorie: "soda"
-  },
-  {
-    id: 5,
-    nom: "Coca-Cola 50cl",
-    prix: 700,
-    image: "https://images.unsplash.com/photo-1572635196237-14b3f281503f?w=400&h=400&fit=crop",
-    categorie: "soda"
-  },
-  {
-    id: 6,
-    nom: "Pepsi 50cl",
-    prix: 650,
-    image: "https://images.unsplash.com/photo-1572635196237-14b3f281503f?w=400&h=400&fit=crop",
-    categorie: "soda"
-  },
-  {
-    id: 7,
-    nom: "Monster Energy",
-    prix: 1000,
-    image: "https://images.unsplash.com/photo-1624555130581-1d9cca1c731d?w=400&h=400&fit=crop",
-    categorie: "energy"
-  },
-  {
-    id: 8,
-    nom: "Ice Tea Pêche",
-    prix: 600,
-    image: "https://images.unsplash.com/photo-1624555130581-1d9cca1c731d?w=400&h=400&fit=crop",
-    categorie: "tea"
-  }
-];
